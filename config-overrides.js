6c466d95a192c2d0d12a37f7fe5c76a496bac596
Sergey Krylov <ksv741@mail.ru>
2022-07-25 08:01:24 +0500

    add absolute paths

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = function override(config) {
+  console.log('Aliases before', config.resolve.alias);
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.alias,
+      'components': path.resolve(__dirname, 'src/components'),
+      'context': path.resolve(__dirname, 'src/context'),
+      'src': path.resolve(__dirname, 'src')
+    },
+  };
+  console.log('CONFIG', config);
+  return config;
+};
